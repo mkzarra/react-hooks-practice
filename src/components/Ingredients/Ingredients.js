4bf21145985a78dd7b54418df6bac9e5a368ff7c
Mike Zarra <mkzarra@Mikes-MacBook-Air.local>
2020-02-13 14:45:09 -0500

    filters and removes ingredients. adds loading indicator

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -6,27 +6,51 @@ import Search from './Search';
 
 function Ingredients() {
   const [userIngredients, setUserIngredients] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const filteredIngredientsHandler = useCallback(filteredIngredients =>
+      setUserIngredients(filteredIngredients),[]);
 
   async function addIngredientHandler(ingredient) {
+    setIsLoading(true);
+
     const response = await fetch('https://react-hooks-practice-48016.firebaseio.com/ingredients.json', {
       method: 'POST',
       body: JSON.stringify(ingredient),
       headers: { "Content-Type": "application/json" }
     });
 
-    const responseData = response.json();
+    setIsLoading(false);
+    const responseData = await response.json();
+
     setUserIngredients(function(prevIngredients) {
       return [...prevIngredients, { id: responseData.name , ...ingredient }];
     });
   }
 
+  async function removeIngredientHandler(ingredientId) {
+    setIsLoading(true);
+
+    await fetch(`https://react-hooks-practice-48016.firebaseio.com/ingredients/${ingredientId}.json`, {
+      method: 'DELETE'
+    });
+
+    setIsLoading(false);
+    
+    setUserIngredients(function(prevIngredients) {
+      return prevIngredients.filter(function(ingredient) {
+        return ingredient.id !== ingredientId;
+      });
+    });
+  }
+
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} />
+      <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading} />
 
       <section>
-        <Search />
-        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
+        <Search onLoadIngredients={filteredIngredientsHandler} />
+        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
       </section>
     </div>
   );
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+  const { onLoadIngredients } = props
+  const [enteredFilter, setEnteredFilter] = useState('');
+  const inputRef = useRef();
+
+  useEffect(function() {
+    const timer = setTimeout(async function() {
+      if (enteredFilter === inputRef.current.value) {
+        const query = (
+          enteredFilter.length === 0
+          ? ''
+          : `?orderBy="title"&equalTo="${enteredFilter}"`
+          );
+
+          const response = await fetch('https://react-hooks-practice-48016.firebaseio.com/ingredients.json' + query);
+          const responseData = await response.json();
+          const loadedIngredients = [];
+          
+          for (const key in responseData) {
+            loadedIngredients.push({
+              id: key,
+              title: responseData[key].title,
+              amount: responseData[key].amount
+            });
+          }
+
+          onLoadIngredients(loadedIngredients);
+        }
+
+        return function() {
+          clearTimeout(timer);
+        }
+      }, 500);
+  }, [enteredFilter, onLoadIngredients, inputRef]);
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input
+            ref={inputRef}
+            type="text"
+            value={enteredFilter}
+            onChange={event => setEnteredFilter(event.target.value)}
+          />
         </div>
       </Card>
     </section>
