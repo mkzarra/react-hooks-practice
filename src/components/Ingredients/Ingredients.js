b5fdc9aff0815d549871b717c2a50feafe05777c
Mike Zarra <mkzarra@Mikes-MacBook-Air.local>
2020-02-13 15:26:26 -0500

    adds error handling

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -2,51 +2,66 @@ import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
+import ErrorModal from '../UI/ErrorModal';
 import Search from './Search';
 
 function Ingredients() {
   const [userIngredients, setUserIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   const filteredIngredientsHandler = useCallback(filteredIngredients =>
       setUserIngredients(filteredIngredients),[]);
 
   async function addIngredientHandler(ingredient) {
     setIsLoading(true);
+    try {
+      const response = await fetch('https://react-hooks-practice-48016.firebaseio.com/ingredients.json', {
+        method: 'POST',
+        body: JSON.stringify(ingredient),
+        headers: { "Content-Type": "application/json" }
+      });
 
-    const response = await fetch('https://react-hooks-practice-48016.firebaseio.com/ingredients.json', {
-      method: 'POST',
-      body: JSON.stringify(ingredient),
-      headers: { "Content-Type": "application/json" }
-    });
+      setIsLoading(false);
+      const responseData = await response.json();
 
-    setIsLoading(false);
-    const responseData = await response.json();
-
-    setUserIngredients(function(prevIngredients) {
-      return [...prevIngredients, { id: responseData.name , ...ingredient }];
-    });
+      setUserIngredients(function(prevIngredients) {
+        return [...prevIngredients, { id: responseData.name , ...ingredient }];
+      });
+    } catch(error) {
+      setError("Something went wrong!\n\n" + error);
+      setIsLoading(false);
+    }
   }
 
   async function removeIngredientHandler(ingredientId) {
     setIsLoading(true);
-
-    await fetch(`https://react-hooks-practice-48016.firebaseio.com/ingredients/${ingredientId}.json`, {
-      method: 'DELETE'
-    });
-
-    setIsLoading(false);
-    
-    setUserIngredients(function(prevIngredients) {
-      return prevIngredients.filter(function(ingredient) {
-        return ingredient.id !== ingredientId;
+    try { 
+      await fetch(`https://react-hooks-practice-48016.firebaseio.com/ingredients/${ingredientId}.json`, {
+        method: 'DELETE'
       });
-    });
+      
+      setIsLoading(false);
+      
+      setUserIngredients(function(prevIngredients) {
+        return prevIngredients.filter(function(ingredient) {
+          return ingredient.id !== ingredientId;
+        });
+      });
+    } catch(error) {
+      setError("Something went wrong!\n\n" + error);
+      setIsLoading(false);
+    }
   }
 
-  return (
+  function clearError() {
+    setError(null);
+  }
+    
+    return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading} />
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      <IngredientForm onAddIngredient={addIngredientHandler} clearError={clearError} loading={isLoading} />
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,22 +1,24 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
+import ErrorModal from '../UI/ErrorModal';
 import './Search.css';
 
 const Search = React.memo(props => {
-  const { onLoadIngredients } = props
+  const { onLoadIngredients, clearError } = props;
   const [enteredFilter, setEnteredFilter] = useState('');
+  const [error, setError] = useState();
   const inputRef = useRef();
 
   useEffect(function() {
     const timer = setTimeout(async function() {
-      if (enteredFilter === inputRef.current.value) {
-        const query = (
-          enteredFilter.length === 0
-          ? ''
-          : `?orderBy="title"&equalTo="${enteredFilter}"`
+      try {
+        if (enteredFilter === inputRef.current.value) {
+          const query = (
+            enteredFilter.length === 0
+            ? ''
+            : `?orderBy="title"&equalTo="${enteredFilter}"`
           );
-
           const response = await fetch('https://react-hooks-practice-48016.firebaseio.com/ingredients.json' + query);
           const responseData = await response.json();
           const loadedIngredients = [];
@@ -28,18 +30,22 @@ const Search = React.memo(props => {
               amount: responseData[key].amount
             });
           }
-
+          
           onLoadIngredients(loadedIngredients);
-        }
-
+        } 
+        
         return function() {
           clearTimeout(timer);
         }
-      }, 500);
-  }, [enteredFilter, onLoadIngredients, inputRef]);
+      } catch(error) {
+        setError("Something went wrong!\n\n" + error);
+      }
+    }, 500);
+  }, [enteredFilter, onLoadIngredients, inputRef, setError]);
 
   return (
     <section className="search">
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
