ea11e324c4a758869181e5baa3c95976a9bb2b91
Mike Zarra <mkzarra@Mikes-MacBook-Air.local>
2020-02-13 12:50:48 -0500

    connects to firebase and adds ingredients with http POST method

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
 function Ingredients() {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  async function addIngredientHandler(ingredient) {
+    const response = await fetch('https://react-hooks-practice-48016.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: { "Content-Type": "application/json" }
+    });
+
+    const responseData = response.json();
+    setUserIngredients(function(prevIngredients) {
+      return [...prevIngredients, { id: responseData.name , ...ingredient }];
+    });
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
       </section>
     </div>
   );
